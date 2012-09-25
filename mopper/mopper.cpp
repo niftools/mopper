085358202275c7f2251e91c2cfab7bcfa2433e1b
Amorilia <amorilia@users.sourceforge.net>
2012-09-25 10:00:29 +0100

    List libraries in #pragma, remove unused ones.

--- mopper/mopper.cpp
+++ mopper/mopper.cpp
@@ -60,11 +60,19 @@
 #include <Common/Base/Config/hkProductFeatures.cxx> 
 
 #pragma comment(lib, "hkBase.lib")
+#pragma comment(lib, "hkCompat.lib")
+#pragma comment(lib, "hkGeometryUtilities.lib")
+#pragma comment(lib, "hkInternal.lib")
+#pragma comment(lib, "hkSceneData.lib")
 #pragma comment(lib, "hkSerialize.lib")
-#pragma comment(lib, "hkpInternal.lib")
-#pragma comment(lib, "hkpUtilities.lib")
+#pragma comment(lib, "hkcdCollide.lib")
+#pragma comment(lib, "hkcdInternal.lib")
 #pragma comment(lib, "hkpCollide.lib")
 #pragma comment(lib, "hkpConstraintSolver.lib")
+#pragma comment(lib, "hkpDynamics.lib")
+#pragma comment(lib, "hkpInternal.lib")
+#pragma comment(lib, "hkpUtilities.lib")
+#pragma comment(lib, "hkpVehicle.lib")
 
 /*-------------------------------------------------------------------------*/
 static void HK_CALL errorReport(const char* msg, void*)
